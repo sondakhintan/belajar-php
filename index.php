d866b892b6cc9bbb68deabab06ab58cc2644f0ec
sondakhintan <sondakhintan18@gmail.com>
2022-10-26 09:52:40 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Intan";
+    $umur = 20;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia <strong>$umur</strong> tahun.<br>";
+
+    $namaAdik = "Wulan";
+    $umurAdik = 18;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia <strong>$umurAdik</strong> tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik ;
+    echo "selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
